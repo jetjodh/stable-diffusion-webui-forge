943f380675ac7abd8478e260674e64eb920f656e
Jodh Singh <jetjodh@gmail.com>
2024-03-31 04:43:04 -0400

    add align transfer

--- extensions-builtin/sd_forge_stylealign/scripts/forge_stylealign.py
+++ extensions-builtin/sd_forge_stylealign/scripts/forge_stylealign.py
@@ -1,9 +1,19 @@
+import numpy as np
 import torch
 import gradio as gr
 
 from modules import scripts
 import ldm_patched.ldm.modules.attention as attention
+from inversion import ddim_inversion
 
+def adain(content_features, style_features):
+    # Calculate mean and std of content features
+    content_mean, content_std = torch.mean(content_features, dim=-1, keepdim=True), torch.std(content_features, dim=-1, keepdim=True)
+    # Calculate mean and std of style features
+    style_mean, style_std = torch.mean(style_features, dim=-1, keepdim=True), torch.std(style_features, dim=-1, keepdim=True)
+    # Normalize content features and apply style mean and std
+    normalized_features = style_std * (content_features - content_mean) / (content_std + 1e-5) + style_mean
+    return normalized_features
 
 def sdp(q, k, v, transformer_options):
     return attention.optimized_attention(q, k, v, heads=transformer_options["n_heads"], mask=None)
@@ -22,14 +32,23 @@ class StyleAlignForForge(scripts.Script):
     def ui(self, *args, **kwargs):
         with gr.Accordion(open=False, label=self.title()):
             shared_attention = gr.Checkbox(label='Share attention in batch', value=False)
+            src_prompt = gr.Textbox(label="Source Prompt", placeholder="Enter a prompt")
 
-        return [shared_attention]
+        return [shared_attention, src_prompt]
 
     def process_before_every_sampling(self, p, *script_args, **kwargs):
         # This will be called before every sampling.
         # If you use highres fix, this will be called twice.
-
         shared_attention = script_args[0]
+        src_prompt = script_args[1]
+
+        if p.init_images:
+            input_image = p.init_images[0]
+            latent_height, latent_width = p.init_latent.shape[2:]
+            image = np.array(input_image.resize((latent_width, latent_height)))
+        
+            z = ddim_inversion(p.sd_model, image, src_prompt, 20, 2)
+
 
         if not shared_attention:
             return
@@ -40,14 +59,21 @@ class StyleAlignForForge(scripts.Script):
             b, f, c = x.shape
             return x.reshape(1, b * f, c)
 
-        def aligned_attention(q, k, v, transformer_options):
+        def aligned_attention(q, k, v, transformer_options, z=None):
             b, f, c = q.shape
+            if z:
+                style_features = z[0:1]
+            else:
+                style_features = q[0:1]
+            q = adain(q, style_features)
+            k = adain(k, style_features)
+            v = adain(v, style_features)
             o = sdp(join(q), join(k), join(v), transformer_options)
             b2, f2, c2 = o.shape
             o = o.reshape(b, b2 * f2 // b, c2)
             return o
 
-        def attn1_proc(q, k, v, transformer_options):
+        def attn1_proc(q, k, v, transformer_options, z=None):
             cond_indices = transformer_options['cond_indices']
             uncond_indices = transformer_options['uncond_indices']
             cond_or_uncond = transformer_options['cond_or_uncond']
@@ -61,13 +87,16 @@ class StyleAlignForForge(scripts.Script):
 
                 if len(indices) > 0:
                     bq, bk, bv = q[indices], k[indices], v[indices]
-                    bo = aligned_attention(bq, bk, bv, transformer_options)
+                    bo = aligned_attention(bq, bk, bv, transformer_options, z)
                     results.append(bo)
 
             results = torch.cat(results, dim=0)
             return results
+        
+        def attn1_proc_wrapper(q, k, v, transformer_options):
+            return attn1_proc(q, k, v, transformer_options, z)
 
-        unet.set_model_replace_all(attn1_proc, 'attn1')
+        unet.set_model_replace_all(attn1_proc_wrapper, 'attn1')
 
         p.sd_model.forge_objects.unet = unet
 
---
+++ extensions-builtin/sd_forge_stylealign/scripts/inversion.py
@@ -0,0 +1,118 @@
+from __future__ import annotations
+from typing import Callable
+from diffusers import StableDiffusionXLPipeline
+from modules.processing import StableDiffusionProcessingTxt2Img
+from modules.sd_samplers import set_samplers
+from ldm_patched.modules.samplers import sampler_object
+from modules.shared import opts
+import torch
+from tqdm import tqdm
+import numpy as np
+
+
+T = torch.Tensor
+TN = T | None
+InversionCallback = Callable[[StableDiffusionXLPipeline, int, T, dict[str, T]], dict[str, T]]
+
+
+def _get_text_embeddings(prompt: str, tokenizer, text_encoder, device):
+    # Tokenize text and get embeddings
+    text_inputs = tokenizer(prompt, padding='max_length', max_length=tokenizer.model_max_length, truncation=True, return_tensors='pt')
+    text_input_ids = text_inputs.input_ids
+
+    with torch.no_grad():
+        prompt_embeds = text_encoder(
+            text_input_ids.to(device),
+            output_hidden_states=True,
+        )
+
+    pooled_prompt_embeds = prompt_embeds[0]
+    prompt_embeds = prompt_embeds.hidden_states[-2]
+    if prompt == '':
+        negative_prompt_embeds = torch.zeros_like(prompt_embeds)
+        negative_pooled_prompt_embeds = torch.zeros_like(pooled_prompt_embeds)
+        return negative_prompt_embeds, negative_pooled_prompt_embeds
+    return prompt_embeds, pooled_prompt_embeds
+
+
+def _encode_text_sdxl(model: StableDiffusionXLPipeline, prompt: str) -> tuple[dict[str, T], T]:
+    device = model._execution_device
+    prompt_embeds, pooled_prompt_embeds, = _get_text_embeddings(prompt, model.sd_model.cond_stage_model.tokenizer, model.text_encoder, device)
+    prompt_embeds_2, pooled_prompt_embeds2, = _get_text_embeddings( prompt, model.tokenizer_2, model.text_encoder_2, device)
+    prompt_embeds = torch.cat((prompt_embeds, prompt_embeds_2), dim=-1)
+    text_encoder_projection_dim = model.text_encoder_2.config.projection_dim
+    add_time_ids = model._get_add_time_ids((1024, 1024), (0, 0), (1024, 1024), torch.float16,
+                                           text_encoder_projection_dim).to(device)
+    added_cond_kwargs = {"text_embeds": pooled_prompt_embeds2, "time_ids": add_time_ids}
+    return added_cond_kwargs, prompt_embeds
+
+
+def _encode_text_sdxl_with_negative(model: StableDiffusionXLPipeline, prompt: str) -> tuple[dict[str, T], T]:
+    added_cond_kwargs, prompt_embeds = _encode_text_sdxl(model, prompt)
+    added_cond_kwargs_uncond, prompt_embeds_uncond = _encode_text_sdxl(model, "")
+    prompt_embeds = torch.cat((prompt_embeds_uncond, prompt_embeds, ))
+    added_cond_kwargs = {"text_embeds": torch.cat((added_cond_kwargs_uncond["text_embeds"], added_cond_kwargs["text_embeds"])),
+                         "time_ids": torch.cat((added_cond_kwargs_uncond["time_ids"], added_cond_kwargs["time_ids"])),}
+    return added_cond_kwargs, prompt_embeds
+
+
+def _encode_image(model: StableDiffusionProcessingTxt2Img, image: np.ndarray) -> T:
+    model.sd_model.first_stage_model.to(dtype=torch.float32)
+    image = torch.from_numpy(image).float() / 255.
+    image = (image * 2 - 1).permute(2, 0, 1).unsqueeze(0)
+    latent = model.sd_model.get_first_stage_encoding(model.sd_model.encode_first_stage(image))
+    model.sd_model.first_stage_model.to(dtype=torch.float16)
+    return latent
+
+
+def _next_step(model: StableDiffusionXLPipeline, model_output: T, timestep: int, sample: T) -> T:
+    sigmas = model.model_sampling.sigmas
+    sigma, sigma_next = sigmas[timestep], sigmas[timestep + 1]
+    
+    next_original_sample = (sample - sigma ** 2 * model_output) / (1 - sigma ** 2) ** 0.5
+    next_sample_direction = sigma_next * model_output
+    next_sample = (1 - sigma_next ** 2) ** 0.5 * next_original_sample + next_sample_direction
+    
+    return next_sample
+
+
+def _get_noise_pred(model: StableDiffusionProcessingTxt2Img, latent: T, t: T, context: T, guidance_scale: float, added_cond_kwargs: dict[str, T]):
+    latents_input = torch.cat([latent] * 2)
+    noise_pred = model.sd_model.model.diffusion_model(latents_input, t, encoder_hidden_states=context, **added_cond_kwargs)["sample"]
+    noise_pred_uncond, noise_prediction_text = noise_pred.chunk(2)
+    noise_pred = noise_pred_uncond + guidance_scale * (noise_prediction_text - noise_pred_uncond)
+    return noise_pred
+
+
+def _ddim_loop(model: StableDiffusionProcessingTxt2Img, z0, prompt, guidance_scale) -> T:
+    all_latent = [z0]
+    added_cond_kwargs, text_embedding = _encode_text_sdxl_with_negative(model, prompt)
+    latent = z0.clone().detach().half()
+
+    sigmas = model.sampler.sigmas
+
+    for i in tqdm(range(model.steps)):
+        t = sigmas[-(i+1)]
+        noise_pred = _get_noise_pred(model, latent, t, text_embedding, guidance_scale, added_cond_kwargs)
+        latent = _next_step(model, noise_pred, t, latent)
+        all_latent.append(latent)
+
+    return torch.cat(all_latent).flip(0)
+
+
+def make_inversion_callback(zts, offset: int = 0) -> [T, InversionCallback]:
+
+    def callback_on_step_end(pipeline: StableDiffusionXLPipeline, i: int, t: T, callback_kwargs: dict[str, T]) -> dict[str, T]:
+        latents = callback_kwargs['latents']
+        latents[0] = zts[max(offset + 1, i + 1)].to(latents.device, latents.dtype)
+        return {'latents': latents}
+    return  zts[offset], callback_on_step_end
+
+
+@torch.no_grad()
+def ddim_inversion(model: StableDiffusionProcessingTxt2Img, x0: np.ndarray, prompt: str, num_inference_steps: int, guidance_scale,) -> T:
+    z0 = _encode_image(model, x0)
+    model.sampler = set_samplers.create_sampler(opts.sampler_name, model.sd_model)
+    model.sampler.make_schedule(ddim_num_steps=num_inference_steps, ddim_eta=0.0, verbose=False)
+    zs = _ddim_loop(model, z0, prompt, guidance_scale)
+    return zs
