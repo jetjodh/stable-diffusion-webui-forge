8d81adbc5384108c50eb920e9c7a277d1d6a2135
Jodh Singh <jetjodh@gmail.com>
2024-03-31 15:42:21 -0400

    fix device allotment

--- extensions-builtin/sd_forge_stylealign/lib/inversion.py
+++ extensions-builtin/sd_forge_stylealign/lib/inversion.py
@@ -34,7 +34,7 @@ def _get_text_embeddings(prompt: str, tokenizer, text_encoder, device):
 
 
 def _encode_text_sdxl(model: StableDiffusionProcessingTxt2Img, prompt: str) -> tuple[dict[str, T], T]:
-    device = model._execution_device
+    device = opts.device
     prompt_embeds, pooled_prompt_embeds, = _get_text_embeddings(prompt, model.cond_stage_model.tokenizer, model.text_encoder, device)
     prompt_embeds_2, pooled_prompt_embeds2, = _get_text_embeddings( prompt, model.tokenizer_2, model.text_encoder_2, device)
     prompt_embeds = torch.cat((prompt_embeds, prompt_embeds_2), dim=-1)
